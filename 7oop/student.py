6bd98d419d91053cdd9ca220606133d16401890a
ice <warpblade@foxmail.com>
2017-08-16 14:35:54 +0800

    类和实例

--- 7oop/student.py
+++ 7oop/student.py
@@ -5,8 +5,16 @@ class Student(object):
     
     def print_score(self):
         print('%s:%s' % (self.name,self.score))
+    
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >=60:
+            return 'B'
+        else:
+            return 'C'
 
-bart = Student('Bart Simpson',59)
-lisa = Student('Lisa Simpson',87)
-bart.print_score()
-lisa.print_score()
+# bart = Student('Bart Simpson',59)
+# lisa = Student('Lisa Simpson',87)
+# bart.print_score()
+# lisa.print_score()
