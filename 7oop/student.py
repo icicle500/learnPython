879392b4f54b0be4e3bb6730b3672eac917beff7
ice <warpblade@foxmail.com>
2017-08-16 19:27:07 +0800

    随便提交一下

--- 7oop/student.py
+++ 7oop/student.py
@@ -1,20 +1,23 @@
 class Student(object):
     def __init__(self,name,score):
-        self.name=name
-        self.score=score
+        self.__name=name
+        self.__score=score
     
+    def get_name(self):
+        return self.__name
+    def get_score(self):
+        return self.__score
+
+    def set_name(self,score):
+        self.__score = score
+
     def print_score(self):
-        print('%s:%s' % (self.name,self.score))
+        print('%s:%s' % (self.__name,self.__score))
     
     def get_grade(self):
-        if self.score >= 90:
+        if self.__score >= 90:
             return 'A'
-        elif self.score >=60:
+        elif self.__score >=60:
             return 'B'
         else:
             return 'C'
-
-# bart = Student('Bart Simpson',59)
-# lisa = Student('Lisa Simpson',87)
-# bart.print_score()
-# lisa.print_score()
