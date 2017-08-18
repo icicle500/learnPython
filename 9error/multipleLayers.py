3accb0a9dbd6637c1ba0a3885d1f4e52cfde4c1d
ice <warpblade@foxmail.com>
2017-08-19 01:16:21 +0800

    一天的一点儿小进度

---
+++ 9error/multipleLayers.py
@@ -0,0 +1,19 @@
+def foo(s):
+    return 10 / int(s)
+
+
+def bar(s):
+    return foo(s) * 2
+
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        print('Error:', e)
+    finally:
+        print('finally...')
+
+main()
+
+# 捕获错误可以跨多层调用
